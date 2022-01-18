d05f99cc26fa1b82675148ca20999a70aa3ddfe3
whgloveucq <whgloveu@hotmail.com>
2022-01-18 20:29:49 +0800

    20220118_evening

--- pushtogithub_home.sh
+++ pushtogithub_home.sh
@@ -13,7 +13,8 @@ git add ${file_name}
 echo "第三步：接着输入git commit -m "$description""
 git commit -m "${description}"
 echo "第四步：先git pull,拉取当前分支最新代码"
-git pull
+git config --global pull.rebase true
+git pull 
 echo "第五步：push到远程master分支上"
 git push -u  origin master
 echo "如有问题，请查看.git 目录下的config文件"
