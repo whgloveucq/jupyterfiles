d4aeda35b62ff3d1dfa62eef393f75320dd69e54
whgloveucq <whgloveu@hotmail.com>
2022-03-18 18:12:32 +0800

    20220318

--- OApractice/sql/test.sql
+++ OApractice/sql/test.sql
@@ -1,3 +1,5 @@
 #create table test_mysql2(name varchar(20) null);
 #select * from test_mysql;
-drop table test_mysql2;
+#drop table test_mysql2;
+create table if not exists test_mysql3(name varchar(20) null);
+create table if not exists test_mysql4(name varchar(20) null);
